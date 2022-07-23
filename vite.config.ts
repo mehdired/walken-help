71236e582773a09156ff3a02cce187c709c2cb5c
redh <mehdi.cheniki@gmail.com>
2022-07-23 00:44:44 -0400

    fix: config vite alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import reactSvgPlugin from 'vite-plugin-react-svg'
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+	plugins: [react(), reactSvgPlugin()],
+	resolve: {
+		alias: {
+			'@components': path.resolve(__dirname, './src/components'),
+			'@features': path.resolve(__dirname, './src/features'),
+			'@assets': path.resolve(__dirname, './src/assets'),
+		},
+	},
 })
