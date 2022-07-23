59c9242e4d6385f1ee5349e015eff9f4c261546f
redh <mehdi.cheniki@gmail.com>
2022-07-22 23:24:27 -0400

    style: configure color stitches

---
+++ stitches.config.ts
@@ -0,0 +1,12 @@
+import { createStitches } from '@stitches/react'
+
+export const { styled, theme } = createStitches({
+	theme: {
+		colors: {
+			common: '#b2b7be',
+			uncommon: '#81c259',
+			rare: '#47acf3',
+			epic: '#826dd4',
+		},
+	},
+})
