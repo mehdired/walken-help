0d843721cd06240a360622254c86c04babac1c4c
redh <mehdi.cheniki@gmail.com>
2022-08-19 14:00:32 -0400

    refactor: use Map for energy type

--- src/types/Energy.ts
+++ src/types/Energy.ts
@@ -1,26 +1,41 @@
-export const energy = {
-	common: {
-		value: 3,
-		cooldown: 120,
-	},
-	uncommon: {
-		value: 4,
-		cooldown: 105,
-	},
-	rare: {
-		value: 6,
-		cooldown: 90,
-	},
-	epic: {
-		value: 8,
-		cooldown: 75,
-	},
-	legendary: {
-		value: 12,
-		cooldown: 60,
-	},
-} as const
+import { RarityTypes } from './Rarity'
 
-type energyKey = keyof typeof energy
+export type EnergyType = { value: number; cooldown: number }
 
-export type EnergyType = typeof energy[energyKey]
+export const energyMap = new Map<RarityTypes, EnergyType>([
+	[
+		'common',
+		{
+			value: 3,
+			cooldown: 120,
+		},
+	],
+	[
+		'uncommon',
+		{
+			value: 4,
+			cooldown: 105,
+		},
+	],
+	[
+		'rare',
+		{
+			value: 6,
+			cooldown: 90,
+		},
+	],
+	[
+		'epic',
+		{
+			value: 8,
+			cooldown: 75,
+		},
+	],
+	[
+		'legendary',
+		{
+			value: 12,
+			cooldown: 60,
+		},
+	],
+])
