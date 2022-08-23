120b33345288bbeb99f916012cfb641c0bdfd925
redh <mehdi.cheniki@gmail.com>
2022-08-22 21:54:56 -0400

    feat: setup metaplex and pollyfill

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 const path = require('path')
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import nodePolyfills from 'rollup-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,14 +13,23 @@ export default defineConfig({
 			'@features': path.resolve(__dirname, './src/features'),
 			'@assets': path.resolve(__dirname, './src/assets'),
 			'@': path.resolve(__dirname, './src'),
+			stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+			events: 'rollup-plugin-node-polyfills/polyfills/events',
+			assert: 'assert',
+			crypto: 'crypto-browserify',
+			util: 'util',
 		},
 	},
 	build: {
-		target: ['es2020'],
+		target: 'es2020',
+		rollupOptions: {
+			plugins: [nodePolyfills({ crypto: true })],
+		},
 	},
 	optimizeDeps: {
 		esbuildOptions: {
 			target: 'es2020',
+			plugins: [NodeGlobalsPolyfillPlugin({ buffer: true })],
 		},
 	},
 })
